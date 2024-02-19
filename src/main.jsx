3cf56afb6c17a7ce20370a2c4bb736cbe0fa7e06
Đỗ Quang Khải <88335848+QuangKhai-dev@users.noreply.github.com>
2024-02-19 15:12:40 +0700

    setup redux toolkit

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
 
 // React-router-dom
+import { BrowserRouter } from 'react-router-dom';
+// redux-toolkit
+import { Provider } from 'react-redux';
+import { store } from './redux/configStore.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>
 );
--- src/routes/useRoutesCustom.jsx
+++ src/routes/useRoutesCustom.jsx
@@ -1,16 +1,7 @@
 import { useRoutes } from 'react-router-dom';
 
 const useRoutesCustom = () => {
-  let element = useRoutes([
-    {
-      path: '/home',
-      element: <h1 className="text-red-500 text-center">Hello</h1>,
-    },
-    {
-      path: '/demo',
-      element: <h1>Hello Demo</h1>,
-    },
-  ]);
+  let element = useRoutes([]);
 
   return element;
 };
